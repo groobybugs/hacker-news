9b76fb04a682c868c679f271610d04989608b6da
groobybugs <groobybugs299@hotmail.com>
2024-11-25 13:29:34 -0600

    [AD-04] Implement Paging with Refresh for Articles
    - Refactored `ArticleRepository` to support paging with both network and local database sources.
    - Integrated `PagingData` for efficient fetching and rendering of articles, with fallback to local cache on network failure.
    - Added `refreshArticles()` method for refreshing the articles list from the API and storing it in the local database.
    - Implemented `deleteArticle()` functionality to remove articles from both local and remote sources.
    - Enhanced UI with `ShimmerEffect` for loading states and improved error handling with retry options.
    - Integrated network connectivity checks to ensure fallback to local data when no internet is available.
    - Added new UI components like `ArticleItem`, `ArticlesList`, and `ErrorContent` for better user experience.
    - Implemented Hilt DI for dependency injection, making repository, network, and database services more modular.
    - Updated AndroidManifest with necessary permissions for network access.

--- app/src/main/java/com/applydigital/hackernews/domain/usecase/GetArticlesUseCase.kt
+++ app/src/main/java/com/applydigital/hackernews/domain/usecase/GetArticlesUseCase.kt
@@ -4,33 +4,40 @@ import androidx.paging.PagingData
 import androidx.paging.filter
 import com.applydigital.hackernews.domain.model.Article
 import com.applydigital.hackernews.domain.repository.ArticleRepository
+import com.applydigital.hackernews.utils.constants.Constants.IO_DISPATCHER
 import javax.inject.Inject
+import javax.inject.Named
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 class GetArticlesUseCase @Inject constructor(
-    private val repository: ArticleRepository
+    private val repository: ArticleRepository,
+    @Named(IO_DISPATCHER) private val ioDispatcher: CoroutineDispatcher,
 ) {
     private val seenArticles = HashSet<String>()
-    private val refresh = MutableSharedFlow<Unit>(replay = 1)
+    private val refresh = MutableSharedFlow<Int?>(replay = 1)
 
     @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(): Flow<PagingData<Article>> =
         refresh
             .flatMapLatest {
                 repository
-                    .also { seenArticles.clear() }
                     .getArticles()
             }
             .map { pagingData ->
-                pagingData.filter { article ->
-                    seenArticles.add(article.id)
-                }
+                pagingData
+                    .also { seenArticles.clear() }
+                    .filter { article ->
+                        seenArticles.add(article.id)
+                    }
             }
+            .flowOn(ioDispatcher)
 
     suspend fun refreshArticles() =
-        refresh.emit(Unit)
+        refresh.emit(null)
 }
